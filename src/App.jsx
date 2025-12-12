13d09cb21b4a5c149ab8398c1eb20651be046e3e
pjha91275 <pjha91275@gmail.com>
2025-12-12 23:47:43 +0530

    Completed 40% of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar.jsx'
+import Manager from './components/Manager.jsx'
 import './App.css'
 
 function App() {
@@ -8,7 +10,8 @@ function App() {
 
   return (
     <>
-      
+      <Navbar />
+      <Manager />
     </>
   )
 }
---
+++ src/components/Manager.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import {useRef, useState, useEffect} from "react";
+
+const Manager = () => {
+   const ref = useRef()
+   const [form, setform] = useState({site:"", username:"", password:""})
+   const [passwordArray, setpasswordArray] = useState([])
+
+   useEffect(() =>{
+    let passwords = localStorage.getItem('passwords');
+    if(passwords){
+      setpasswordArray(JSON.parse(passwords))
+    }
+   }, [])
+
+   const showPassword = () =>{
+     alert("show the password");
+     console.log(ref.current.src)
+     if(ref.current.src.includes("icons/eyecross.png")){
+      ref.current.src = "icons/eye.png"
+     }
+     else{
+      ref.current.src = "icons/eyecross.png"
+     }
+   }
+
+   const savePassword = () => {
+     setpasswordArray([...passwordArray, form])
+     localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
+     console.log([...passwordArray, form])
+   }
+
+   const handleChange = (e) => {
+     setform({...form, [e.target.name]: e.target.value})
+   }
+   
+  return (
+    <>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+              
+      <div className="mycontainer">
+        <h1 className="text-4xl text font-bold text-center">
+          <span className="text-green-500"> &lt;</span>
+        <span>Iron</span>
+         <span className="text-green-500">Vault/&gt;</span>
+          </h1>
+        <p className="text-green-900 text-lg text-center">Your own password Manager</p>
+
+        <div className="flex flex-col p-4 text-black gap-8 items-center">
+          <input value={form.site} onChange={handleChange}  placeholder="Enter Website URL"
+            className="rounded-full border border-green-500 w-full p-4 py-1"
+            type="text"
+            name="site"
+            id=""
+          />
+          <div className="flex w-full justify-between gap-8">
+            <input value={form.username} onChange={handleChange} placeholder="Enter username"
+              className="rounded-full border border-green-500 w-full p-4 py-1"
+              type="text"
+              name="username"
+              id=""
+            />
+
+            <div className="relative">
+                <input value={form.password} onChange={handleChange} placeholder="Enter password"
+              className="rounded-full border border-green-500 w-full p-4 py-1"
+              type="text"
+              name="password"
+              id=""
+            />
+              <span className="absolute right-[3px] top-[4px] cursor-pointer onClick={showPassword}" >
+                <img ref={ref} className="p-1" width={26} src="icons/eye.png" alt="eye" />
+              </span>
+            </div>
+            
+          </div>
+
+          <button className="flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900" onClick={savePassword}>
+            <lord-icon
+    src="https://cdn.lordicon.com/efxgwrkc.json"
+    trigger="hover"
+    >
+</lord-icon>
+            Add password
+          </button>
+        </div>
+
+        <div className="passwords">
+          <h2 className='font-bold text-2xl py-4'>Your Passwords</h2>
+          {passwordArray.length === 0 && <div> No passwords to show</div>}
+          {passwordArray.length != 0 && <table className="table-auto w-full rounded-md overflow-hidden">
+  <thead className='bg-green-800 text-white'>
+    <tr>
+      <th className='py-2'>Site</th>
+      <th className='py-2'>Username</th>
+      <th className='py-2'>Password</th>
+    </tr>
+  </thead>
+  <tbody className='bg-green-100'>
+    {passwordArray.map((item, index) =>{
+      return <tr key={index}>
+      <td className='py-2 border border-white text-center w-32'>{item.site}</td>
+      <td className='py-2 border border-white text-center w-32'>{item.username}</td>
+      <td className='py-2 border border-white text-center w-32'>{item.password}</td>
+    </tr>
+    })}
+    
+    
+  </tbody>
+</table>}
+        </div>
+      </div>
+          
+      
+    </>
+  );
+};
+
+export default Manager;
